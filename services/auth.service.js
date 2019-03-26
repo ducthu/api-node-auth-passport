85c25783a7f91dbb464e9ec80e2a17ea4eb7b36a
ducthu <mail@ducthu.com>
2019-03-26 18:32:26 +0100

    create auth controller

---
+++ services/auth.service.js
@@ -0,0 +1,16 @@
+const config = require('../config/db');
+const db = require('../_helpers/database');
+const User = db.User;
+
+module.exports = {
+    login,
+    register
+};
+
+async function login(){
+
+};
+
+async function register(){
+
+};
